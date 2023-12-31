c95f27e93ff761a33885bb69a9ce374f9551a866
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2023-12-31 19:32:31 +0530

    Add files via upload

---
+++ Admin/edit_author.php
@@ -0,0 +1,70 @@
+<?php
+
+	session_start();
+	if(!isset($_SESSION['email']))
+{
+	die("Access denied");
+}
+	#fetch data from database
+	$connection = mysqli_connect("localhost","root","");
+	$db = mysqli_select_db($connection,"lms");
+	$author_id = "";
+	$author_name = "";
+	$query = "select * from authors where author_id = $_GET[aid]";
+	$query_run = mysqli_query($connection,$query);
+	while ($row = mysqli_fetch_assoc($query_run)){
+		$author_name = $row['author_name'];
+		$author_id = $row['author_id'];
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashboard</title>
+	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1/css/bootstrap.min.css">
+  	<script type="text/javascript" src="../bootstrap-4.4.1/js/juqery_latest.js"></script>
+  	<script type="text/javascript" src="../bootstrap-4.4.1/js/bootstrap.min.js"></script>
+	  <style>
+		body {
+			background: rgb(238, 174, 202);
+			background: -moz-radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(195, 177, 219, 1) 38%, rgba(148, 187, 233, 1) 100%);
+			background: -webkit-radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(195, 177, 219, 1) 38%, rgba(148, 187, 233, 1) 100%);
+			background: radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(195, 177, 219, 1) 38%, rgba(148, 187, 233, 1) 100%);
+			filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#eeaeca", endColorstr="#94bbe9", GradientType=1);
+		}
+	</style>
+</head>
+<body>
+<?php include('navbar.php')?>
+	<span><marquee style="background-color:violet"><b>Dear,<?php echo $_SESSION['name'];?> Edit the Author Now!!!
+	</b></marquee></span><br><br>
+		<center><h4><b><u>Edit Book</u></b></h4><br></center>
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<form action="" method="post">
+					<div class="form-group">
+						<label for="name">Author Name:</label>
+						<input type="text" class="form-control" name="author_name" value="<?php echo $author_name; ?>" required>
+					</div>
+					<button type="submit" name="update_author" class="btn btn-primary">Update Author</button>
+				</form>
+			</div>
+			<div class="col-md-4"></div>
+		</div>
+</body>
+</html>
+<?php
+	if(isset($_POST['update_author'])){
+		include("db_connect.php");
+		$query = "update authors set author_name = '$_POST[author_name]' where author_id = $_GET[aid]";
+		$query_run = mysqli_query($connection,$query);
+		?>
+			<script type="text/javascript">
+		alert("Author edited Successfully");
+	</script>
+	<?php
+		header("location:manage_author.php");
+	}
+?>
---
+++ Admin/edit_book.php
@@ -0,0 +1,91 @@
+<?php
+
+	session_start();
+	if(!isset($_SESSION['email']))
+{
+	die("Access denied");
+}
+	#fetch data from database
+	$connection = mysqli_connect("localhost","root","");
+	$db = mysqli_select_db($connection,"lms");
+	$book_name = "";
+	$book_no = "";
+	$author_id = "";
+	$cat_id = "";
+	$book_price = "";
+	$query = "select * from books where book_no = $_GET[bn]";
+	$query_run = mysqli_query($connection,$query);
+	while ($row = mysqli_fetch_assoc($query_run)){
+		$book_name = $row['book_name'];
+		$book_no = $row['book_no'];
+		$author_id = $row['author_id'];
+		$cat_id = $row['cat_id'];
+		$book_price = $row['book_price'];
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashboard</title>
+	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1/css/bootstrap.min.css">
+  	<script type="text/javascript" src="../bootstrap-4.4.1/js/juqery_latest.js"></script>
+  	<script type="text/javascript" src="../bootstrap-4.4.1/js/bootstrap.min.js"></script>
+	  <style>
+		body {
+			background: rgb(238, 174, 202);
+			background: -moz-radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(195, 177, 219, 1) 38%, rgba(148, 187, 233, 1) 100%);
+			background: -webkit-radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(195, 177, 219, 1) 38%, rgba(148, 187, 233, 1) 100%);
+			background: radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(195, 177, 219, 1) 38%, rgba(148, 187, 233, 1) 100%);
+			filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#eeaeca", endColorstr="#94bbe9", GradientType=1);
+		}
+	</style>
+</head>
+<body>
+<?php include('navbar.php')?>
+	<span><marquee style="background-color:violet"><b>Dear,<?php echo $_SESSION['name'];?> Edit the Book Now!!!</b></marquee></span><br><br>
+		<center><h4><b><u>Edit Book</u><b></h4><br></center>
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<form action="" method="post">
+					<div class="form-group">
+						<label for="mobile">Book Number:</label>
+						<input type="text" name="book_no" value="<?php echo $book_no;?>" class="form-control" disabled required>
+					</div>
+					<div class="form-group">
+						<label for="email">Book Name:</label>
+						<input type="text" name="book_name" value="<?php echo $book_name;?>" class="form-control" required>
+					</div>
+					<div class="form-group">
+						<label for="mobile">Author ID:</label>
+						<input type="text" name="author_id" value="<?php echo $author_id;?>" class="form-control" required>
+					</div>
+					<div class="form-group">
+						<label for="mobile">Category ID:</label>
+						<input type="text" name="cat_id" value="<?php echo $cat_id;?>" class="form-control" required>
+					</div>
+					<div class="form-group">
+						<label for="mobile">Book Price:</label>
+						<input type="text" name="book_price" value="<?php echo $book_price;?>" class="form-control" required>
+					</div>
+					<button type="submit" name="update" class="btn btn-primary">Update Book</button>
+				</form>
+			</div>
+			<div class="col-md-4"></div>
+		</div>
+</body>
+</html>
+<?php
+	if(isset($_POST['update'])){
+		include("db_connect.php");
+		$query = "update books set book_name = '$_POST[book_name]',author_id = $_POST[author_id],cat_id = $_POST[cat_id],book_price = $_POST[book_price] where book_no = $_GET[bn]";
+		$query_run = mysqli_query($connection,$query);
+		?>
+			<script type="text/javascript">
+		alert("Book edited Successfully");
+	</script>
+	<?php
+		header("location:manage_book.php");
+	}
+?>
