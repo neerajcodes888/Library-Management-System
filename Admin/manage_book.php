1e95d01373f7902503c0b9dde125bfb125e65368
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-01-01 19:11:00 +0530

    Add files via upload

---
+++ Admin/manage_book.php
@@ -0,0 +1,136 @@
+<?php
+
+require("functions.php");
+session_start();
+if(!isset($_SESSION['email']))
+{
+	die("Access denied");
+}
+#fetch data from database
+$connection = mysqli_connect("localhost","root","");
+$db = mysqli_select_db($connection,"lms");
+$name = "";
+$email = "";
+$mobile = "";
+$query = "select * from admins where email = '$_SESSION[email]'";
+$query_run = mysqli_query($connection, $query);
+while ($row = mysqli_fetch_assoc($query_run)) {
+	$name = $row['name'];
+	$email = $row['email'];
+	$mobile = $row['mobile'];
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Manage Book</title>
+	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1/css/bootstrap.min.css">
+	<script type="text/javascript" src="../bootstrap-4.4.1/js/juqery_latest.js"></script>
+	<script type="text/javascript" src="../bootstrap-4.4.1/js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+		function alertMsg() {
+			alert("Book added successfully...");
+			window.location.href = "admin_dashboard.php";
+		}
+	</script>
+	<style>
+		body {
+			background: rgb(238, 174, 202);
+			background: -moz-radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(195, 177, 219, 1) 38%, rgba(148, 187, 233, 1) 100%);
+			background: -webkit-radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(195, 177, 219, 1) 38%, rgba(148, 187, 233, 1) 100%);
+			background: radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(195, 177, 219, 1) 38%, rgba(148, 187, 233, 1) 100%);
+			filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#eeaeca", endColorstr="#94bbe9", GradientType=1);
+		}
+	</style>
+</head>
+
+<body>
+<?php include('navbar.php')?>
+	<nav class="navbar navbar-expand-lg navbar-light" style="background-color: salmon">
+		<div class="container-fluid">
+
+			<ul class="nav navbar-nav navbar-center">
+				<li class="nav-item">
+					<a class="nav-link" href="admin_dashboard.php">Dashboard</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown">Books </a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="add_book.php">Add New Book</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="manage_book.php">Manage Books</a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown">Category </a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="add_cat.php">Add New Category</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="manage_cat.php">Manage Category</a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown">Authors</a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="add_author.php">Add New Author</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="manage_author.php">Manage Author</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="issue_book.php">Issue Book</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<span>
+		<marquee style="background-color:violet"><b>
+		Dear,<?php echo $_SESSION['name'];?> Please,Take one of the action whether edit the book or remove it!!!</b>
+		</marquee>
+	</span><br><br>
+	<center>
+		<h4><b><u>Manage Books</u></b></h4><br>
+	</center>
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-8">
+			<table class="table table-bordered table-hover">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Author</th>
+						<th>Category</th>
+						<th>ISBN No.</th>
+						<th>Price</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<?php
+		
+				$query = "select * from books";
+				$query_run = mysqli_query($connection, $query);
+				while ($row = mysqli_fetch_assoc($query_run)) {
+				?>
+					<tr>
+						<td><?php echo $row['book_name']; ?></td>
+						<td><?php echo $row['author_id']; ?></td>
+						<td><?php echo $row['cat_id']; ?></td>
+						<td><?php echo $row['book_no']; ?></td>
+						<td><?php echo $row['book_price']; ?></td>
+						<td>
+							<button class="btn" name=""><a href="edit_book.php?bn=<?php echo $row['book_no']; ?>"" style="color:green">Edit</a></button>
+							<button class="btn"><a href="delete_book.php?bn=<?php echo $row['book_no']; ?>"" style="color:red">Delete</a></button>
+						</td>
+					</tr>
+				<?php
+				}
+				?>
+			</table>
+		</div>
+		<div class="col-md-2"></div>
+	</div>
+</body>
+
+</html>
---
+++ Admin/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="admin_dashboard.php">Library Management System (LMS)</a>
+			</div>
+			<font style="color: white"><span><strong>Welcome: <?php echo $_SESSION['name'];?></strong></span></font>
+			<font style="color: white"><span><strong>Email: <?php echo $_SESSION['email'];?></strong></font>
+		    <ul class="nav navbar-nav navbar-right">
+		      <li class="nav-item dropdown">
+	        	<a class="nav-link dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['name'];?></a>
+	        	<div class="dropdown-menu">
+	        		<a class="dropdown-item" href="view_profile.php">View Profile</a>
+	        		<div class="dropdown-divider"></div>
+	        		<a class="dropdown-item" href="edit_profile.php">Edit Profile</a>
+	        		<div class="dropdown-divider"></div>
+	        		<a class="dropdown-item" href="change_password.php">Change Password</a>
+	        	</div>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="../logout.php">Logout</a>
+		      </li>
+		    </ul>
+		</div>
+	</nav>
