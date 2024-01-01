d9565bfbbe8f23914c5a4dd68cb5be55fa43fa88
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-01-01 18:52:59 +0530

    Add files via upload

---
+++ Admin/index.php
@@ -0,0 +1,128 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>LMS | Login</title>
+	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1/css/bootstrap.min.css">
+	<script type="text/javascript" src="./bootstrap-4.4.1/js/juqery_latest.js"></script>
+	<script type="text/javascript" src="./bootstrap-4.4.1/js/bootstrap.min.js"></script>
+  	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>  
+	<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.min.css'></link>
+</head>
+<style type="text/css">
+	#main_content {
+		padding: 50px;
+		background-image: linear-gradient(-225deg, #E3FDF5 0%, #FFE6FA 100%);
+		background-image: linear-gradient(to top, #a8edea 0%, #fed6e3 100%);
+		background-attachment: fixed;
+		background-repeat: no-repeat;
+		font-family: 'Vibur', cursive;
+		font-family: 'Abel', sans-serif;
+		opacity: .95;
+
+	}
+
+	#side_bar {
+		background-color: lightgrey;
+		padding: 50px;
+		width: 300px;
+		height: 450px;
+	}
+</style>
+
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="../index.php">Library Management System (LMS)</a>
+			</div>
+
+			<ul class="nav navbar-nav navbar-right">
+				<li class="nav-item">
+					<a class="nav-link" href="../index.php">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="../user_login.php"> User Login</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="../signup.php"></span>Register</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="index.php">Admin Login</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="../about_us.php">About us</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<marquee style="background-color:lightblue"><b>
+Hello , Admins!! Here is the login for your Portal.Please Enter correct Credentials to log in. Incase of any error report ASAP!!!
+		</b></marquee>
+	</span>
+	<div class="row">
+		<div class="col-md-4" id="side_bar">
+			<h5>Library Timing</h5>
+			<ul>
+				<li>Opening: 8:00 AM</li>
+				<li>Closing: 8:00 PM</li>
+				<li>(Sunday Off)</li>
+			</ul>
+			<h5>What We provide ?</h5>
+			<ul>
+				<li>Full furniture</li>
+				<li>Free Wi-fi</li>
+				<li>News Papers</li>
+				<li>Discussion Room</li>
+				<li>RO Water</li>
+				<li>Peacefull Environment</li>
+			</ul>
+		</div>
+		<div class="col-md-8" id="main_content">
+			<center>
+			<img src="../images/admin.png" alt="user login" width="120" height="100">
+				<h3><b><u>Admin Login Form</u></b></h3>
+			</center>
+			<form action="" method="post">
+				<div class="form-group">
+					<label for="email">Email ID:</label>
+					<input type="text" name="email" class="form-control" required>
+				</div>
+				<div class="form-group">
+					<label for="password">Password:</label>
+					<input type="password" name="password" class="form-control" required>
+				</div>
+				<center><button type="submit" name="login" class="btn btn-primary">Login</button></center>
+			</form>
+			<?php
+			if (isset($_POST['login'])) {
+				$connection = mysqli_connect("localhost","root","");
+				$db = mysqli_select_db($connection,"lms");
+				$query = "select * from admins where email = '$_POST[email]'";
+				$query_run = mysqli_query($connection, $query);
+				while ($row = mysqli_fetch_assoc($query_run)) {
+					if ($row['email'] == $_POST['email']) {
+						if ($row['password'] == $_POST['password']) {
+							$_SESSION['name'] = $row['name'];
+							$_SESSION['email'] = $row['email'];
+							header("Location: admin_dashboard.php");
+						} else {
+							?>
+							<br><br>
+							<center><span class="alert-danger">Wrong Password !!</span></center>
+							<?php
+						}
+					}
+				}
+			}
+			?>
+		</div>
+	</div>
+	<?php include('../footer.php') ?>
+</body>
+
+</html>
---
+++ Admin/issue_book.php
@@ -0,0 +1,160 @@
+<?php
+session_start();
+if (!isset($_SESSION['email'])) {
+	header("location:user_login.php");
+}
+
+#fetch data from database
+$connection = mysqli_connect("localhost", "root", "");
+$db = mysqli_select_db($connection, "lms");
+$book_name = "";
+$author_name = "";
+$category = "";
+$book_no = "";
+$price = "";
+$quantity="";
+$query = "select books.book_id,books.book_name,books.book_no,book_price,books.quantity,authors.author_name from books left join authors on books.author_id = authors.author_id";
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>All Reg Books</title>
+	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1/css/bootstrap.min.css">
+	<script type="text/javascript" src="../bootstrap-4.4.1/js/juqery_latest.js"></script>
+	<script type="text/javascript" src="../bootstrap-4.4.1/js/bootstrap.min.js"></script>
+	<style>
+		body {
+			background-image: linear-gradient(-120deg, #d4fc79 0%, #96e6a1 100%);
+			background-image: linear-gradient(to right, #fc5c7d, #6a82fb);
+			background-attachment: fixed;
+			background-repeat: no-repeat;
+		}
+	</style>
+	<?php include('navbar.php') ?>
+	<span>
+		<marquee style="background-color:lightblue">
+			<b>
+				<?php echo $_SESSION['name']; ?> Here is the list Of available books in the library. Have a look and take
+				which one you need and download!!!
+			</b>
+		</marquee>
+	</span>
+	<br><br>
+	<div class="container">
+		<form method="post">
+			<input type="text" placeholder="Search Books/Authors" name=search>
+			<button class="btn btn-dark btn-sm" name="submit">Search</button>
+		</form>
+	</div>
+	<center>
+		<h4><u><b>Registered Book's Details</b></u></h4><br>
+	</center>
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-8">
+			<form>
+				<table class="table-bordered" width="900px" style="text-align: center">
+					<tr>
+						<th>Book ID</th>
+						<th>Name</th>
+						<th>Author</th>
+						<th>Price</th>
+						<th>Number</th>
+						<th>Quantity</th>
+					</tr>
+
+					<?php
+					$query_run = mysqli_query($connection, $query);
+					while ($row = mysqli_fetch_assoc($query_run)) {
+						?>
+						<tr>
+							<td>
+								<?php echo $row['book_id']; ?>
+							</td>
+							<td>
+								<?php echo $row['book_name']; ?>
+							</td>
+							<td>
+								<?php echo $row['author_name']; ?>
+							</td>
+							<td>
+								<?php echo $row['book_price']; ?>
+							</td>
+							<td>
+								<?php echo $row['book_no']; ?>
+							</td>
+							<td>
+								<?php echo $row['quantity']; ?>
+							</td>
+						</tr>
+
+						<?php
+					}
+					?>
+				</table>
+				<br>
+				<center>
+
+					<a class="btn btn-warning" href="downloads/down_avail.php" target="_blank">Download list</a>
+				</center>
+			</form>
+		</div>
+		<div class="col-md-2"></div>
+	</div>
+	<br>
+	<center>
+		<h4><u><b>Searched Book's Details</b></u></h4><br>
+	</center>
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-8">
+			<form>
+				<table class="table-bordered" width="900px" style="text-align: center">
+					<tr>
+						<th>Book ID</th>
+						<th>Name</th>
+						<th>Author</th>
+						<th>Price</th>
+						<th>Number</th>
+						<th>Quantity</th>
+						<th>Action</th>
+					</tr>
+
+					<?php
+					if (isset($_POST['submit'])) {
+						$sql = "SELECT * from books,authors where books.author_id=authors.author_id and (books.book_name like '%$_POST[search]%' or authors.author_name like '%$_POST[search]%')";
+						$query_run = mysqli_query($connection, $sql);
+					}
+					while ($row = mysqli_fetch_assoc($query_run)) {
+						?>
+						<tr>
+							<td>
+								<?php echo $row['book_id']; ?>
+							</td>
+							<td>
+								<?php echo $row['book_name']; ?>
+							</td>
+							<td>
+								<?php echo $row['author_name']; ?>
+							</td>
+							<td>
+								<?php echo $row['book_price']; ?>
+							</td>
+							<td>
+								<?php echo $row['book_no']; ?>
+							</td>
+							<td>
+								<?php echo $row['quantity']; ?>
+							</td>
+							<td><button class="btn" name=""><a href="issue.php?bn=<?php echo $row['book_no']; ?>"" style="
+										color:green">Issue</a></button></td>
+						</tr>
+
+						<?php
+					}
+					?>
+				</table>
+				</body>
+</html>
