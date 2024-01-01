4b5adabb2cd1c89dff301a3f94c39379a67a16e1
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-01-01 13:30:24 +0530

    Add files via upload

---
+++ Admin/feed_form.php
@@ -0,0 +1,111 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['email']))
+{
+	die("Access denied");
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Issue Book</title>
+	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1/css/bootstrap.min.css">
+	<script type="text/javascript" src="../bootstrap-4.4.1/js/juqery_latest.js"></script>
+	<script type="text/javascript" src="../bootstrap-4.4.1/js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+		function alertMsg() {
+			alert("Book added successfully...");
+			window.location.href = "admin_dashboard.php";
+		}
+	</script>
+		<style>
+				body{
+			background: rgb(238,174,202);
+background: -moz-radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(195,177,219,1) 38%, rgba(148,187,233,1) 100%);
+background: -webkit-radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(195,177,219,1) 38%, rgba(148,187,233,1) 100%);
+background: radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(195,177,219,1) 38%, rgba(148,187,233,1) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#eeaeca",endColorstr="#94bbe9",GradientType=1);
+		}
+		</style>
+</head>
+
+<body>
+<?php include('navbar.php')?>
+	<nav class="navbar navbar-expand-lg navbar-light" style="background-color: salmon">
+		<div class="container-fluid">
+
+			<ul class="nav navbar-nav navbar-center">
+				<li class="nav-item">
+					<a class="nav-link" href="admin_dashboard.php">Dashboard</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown">Books </a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="add_book.php">Add New Book</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="manage_book.php">Manage Books</a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown">Category </a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="add_cat.php">Add New Category</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="manage_cat.php">Manage Category</a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-toggle="dropdown">Authors</a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="add_author.php">Add New Author</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="manage_author.php">Manage Author</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="issue_book.php">Issue Book</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<span>
+		<marquee style="background-color:violet">
+Dear Dear,<?php echo $_SESSION['name'];?>  Please give response of the feedback given by the student.!!!
+		</marquee>
+	</span><br><br>
+	<center>
+		<h4>Feedback Response</h4><br>
+	</center>
+	<div class="row">
+		<div class="col-md-4"></div>
+		<div class="col-md-4">
+			<form action="" method="post">
+            <div class="form-group">
+                            <textarea name="feedback" class="form-control" placeholder="Your Response*" style="width: 100%; height: 300px;"></textarea>
+                        </div>
+                
+				<button type="submit" name="send_feed" class="btn btn-primary">Send</button>
+			</form>
+		</div>
+		<div class="col-md-4"></div>
+	</div>
+</body>
+
+</html>
+
+<?php
+if (isset($_POST['send_feed'])) {
+	$connection = mysqli_connect("localhost","root","");
+	$db = mysqli_select_db($connection,"lms");
+	$query = "update feedback set response='$_POST[feedback]' where id=$_GET[id_n]";
+	$query_run = mysqli_query($connection, $query);
+	?>
+	<script type="text/javascript">
+alert("Feedback Responded Successfully!!!");
+</script>
+<?php
+}
+?>
---
+++ Admin/functions.php
@@ -0,0 +1,89 @@
+<?php
+// if(!isset($_SESSION['email']))
+// {
+// 	die("Access denied");
+// }
+	function get_author_count(){
+		$connection = mysqli_connect("localhost","root","");
+		$db = mysqli_select_db($connection,"lms");
+		$author_count = 0;
+		$query = "select count(*) as author_count from authors";
+		$query_run = mysqli_query($connection,$query);
+		while ($row = mysqli_fetch_assoc($query_run)){
+			$author_count = $row['author_count'];
+		}
+		return($author_count);
+	}
+
+	function get_user_count(){
+		$connection = mysqli_connect("localhost","root","");
+		$db = mysqli_select_db($connection,"lms");
+		$user_count = 0;
+		$query = "select count(*) as user_count from users";
+		$query_run = mysqli_query($connection,$query);
+		while ($row = mysqli_fetch_assoc($query_run)){
+			$user_count = $row['user_count'];
+		}
+		return($user_count);
+	}
+
+	function get_book_count(){
+		$connection = mysqli_connect("localhost","root","");
+		$db = mysqli_select_db($connection,"lms");
+		$book_count = 0;
+		$query = "select count(*) as book_count from books";
+		$query_run = mysqli_query($connection,$query);
+		while ($row = mysqli_fetch_assoc($query_run)){
+			$book_count = $row['book_count'];
+		}
+		return($book_count);
+	}
+
+	function get_issue_book_count(){
+		$connection = mysqli_connect("localhost","root","");
+		$db = mysqli_select_db($connection,"lms");;
+		$issue_book_count = 0;
+		$query = "select count(*) as issue_book_count from issued_books";
+		$query_run = mysqli_query($connection,$query);
+		while ($row = mysqli_fetch_assoc($query_run)){
+			$issue_book_count = $row['issue_book_count'];
+		}
+		return($issue_book_count);
+	}
+
+	function get_category_count(){
+		$connection = mysqli_connect("localhost","root","");
+		$db = mysqli_select_db($connection,"lms");
+		$cat_count = 0;
+		$query = "select count(*) as cat_count from category";
+		$query_run = mysqli_query($connection,$query);
+		while ($row = mysqli_fetch_assoc($query_run)){
+			$cat_count = $row['cat_count'];
+		}
+		return($cat_count);
+	}
+	function not_return_book_count(){
+		$connection = mysqli_connect("localhost","root","");
+		$db = mysqli_select_db($connection,"lms");
+		$not_return_book_count = 0;
+		$query = "select count(*) as not_return_book_count from issued_books where current_date > adddate(issue_date,30)";
+		$query_run = mysqli_query($connection,$query);
+		while ($row = mysqli_fetch_assoc($query_run)){
+			$not_return_book_count = $row['not_return_book_count'];
+		}
+		return($not_return_book_count);
+	}
+	function get_request_count(){
+		$connection = mysqli_connect("localhost","root","");
+		$db = mysqli_select_db($connection,"lms");
+		$request_count = 0;
+		$query = "select count(*) as request_count from request_books";
+		$query_run = mysqli_query($connection,$query);
+		while ($row = mysqli_fetch_assoc($query_run)){
+			$request_count = $row['request_count'];
+		}
+		return($request_count);
+	}
+
+
+?>
