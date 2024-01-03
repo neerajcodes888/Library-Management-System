9b0f55fb43cf6515061f3ea8b2d94991db89516e
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-01-03 20:34:42 +0530

    Add files via upload

---
+++ User/about_us.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>About Us</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <style>
+        body {
+            background-image: linear-gradient(-225deg, #E3FDF5 0%, #FFE6FA 100%);
+            background-image: linear-gradient(to top, #a8edea 0%, #fed6e3 100%);
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+        }
+
+        .row.heading h2 {
+            color: #fff;
+            font-size: 52.52px;
+            line-height: 95px;
+            font-weight: 400;
+            text-align: center;
+            margin: 0 0 40px;
+            padding-bottom: 20px;
+            text-transform: uppercase;
+        }
+
+        ul {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+        }
+
+        .heading.heading-icon {
+            display: block;
+        }
+
+        .padding-lg {
+            display: block;
+            padding-top: 60px;
+            padding-bottom: 60px;
+        }
+
+        .practice-area.padding-lg {
+            padding-bottom: 55px;
+            padding-top: 55px;
+        }
+
+        .practice-area .inner {
+            border: 1px solid #999999;
+            text-align: center;
+            margin-bottom: 28px;
+            padding: 40px 25px;
+        }
+
+        .our-webcoderskull .cnt-block:hover {
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+            border: 0;
+        }
+
+        .practice-area .inner h3 {
+            color: #3c3c3c;
+            font-size: 24px;
+            font-weight: 500;
+            font-family: 'Poppins', sans-serif;
+            padding: 10px 0;
+        }
+
+        .practice-area .inner p {
+            font-size: 14px;
+            line-height: 22px;
+            font-weight: 400;
+        }
+
+        .practice-area .inner img {
+            display: inline-block;
+        }
+
+
+        .our-webcoderskull {
+            background: url("http://www.webcoderskull.com/img/right-sider-banner.png") no-repeat center top / cover;
+
+        }
+
+        .our-webcoderskull .cnt-block {
+            float: left;
+            width: 100%;
+            background: #fff;
+            padding: 30px 20px;
+            text-align: center;
+            border: 2px solid #d5d5d5;
+            margin: 0 0 28px;
+        }
+
+        .our-webcoderskull .cnt-block figure {
+            width: 148px;
+            height: 148px;
+            border-radius: 100%;
+            display: inline-block;
+            margin-bottom: 15px;
+        }
+
+        .our-webcoderskull .cnt-block img {
+            width: 148px;
+            height: 148px;
+            border-radius: 100%;
+        }
+
+        .our-webcoderskull .cnt-block h3 {
+            color: #2a2a2a;
+            font-size: 20px;
+            font-weight: 500;
+            padding: 6px 0;
+            text-transform: uppercase;
+        }
+
+        .our-webcoderskull .cnt-block h3 a {
+            text-decoration: none;
+            color: #2a2a2a;
+        }
+
+        .our-webcoderskull .cnt-block h3 a:hover {
+            color: #337ab7;
+        }
+
+        .our-webcoderskull .cnt-block p {
+            color: #2a2a2a;
+            font-size: 13px;
+            line-height: 20px;
+            font-weight: 400;
+        }
+
+        .our-webcoderskull .cnt-block .follow-us {
+            margin: 20px 0 0;
+        }
+
+        .our-webcoderskull .cnt-block .follow-us li {
+            display: inline-block;
+            width: auto;
+            margin: 0 5px;
+        }
+
+        .our-webcoderskull .cnt-block .follow-us li .fa {
+            font-size: 24px;
+            color: #767676;
+        }
+
+        .our-webcoderskull .cnt-block .follow-us li .fa:hover {
+            color: #025a8e;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">Library Management System (LMS)</a>
+            </div>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="user_login.php"> User Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="signup.php"></span>Register</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin/index.php">Admin Login</a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a class="nav-link" href="about_us.php">About Us</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <span>
+        <marquee style="background-color:lightblue"><b>
+                This about-us page will tell you ,more about us like our vision and also the members of LMS(Officils)!!!
+                Incase of any doubt,or any kind of help ,please contact the members..
+            </b>
+        </marquee>
+    </span>
+    <section class="about-us py-5 " id="about-us">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1 class='text-success'>Welcome!</h1>
+                    <h2>Know More About Us</h2>
+                    <hr>
+                    <p>Welcome to our Library Management System, a state-of-the-art software application designed to
+                        streamline library operations and enhance the user experience for both library staff and
+                        patrons. Our system is dedicated to empowering libraries to efficiently manage their resources,
+                        deliver exceptional services, and foster a love for learning and knowledge.</p>
+                    <h2>Our Mission:</h2>
+                    <p>
+                        At Library Management System, our mission is to revolutionize the way libraries operate by
+                        providing an innovative and comprehensive solution that simplifies complex tasks and optimizes
+                        day-to-day operations. We are committed to supporting libraries in their mission to promote
+                        literacy, education, and community engagement.</p>
+                    <h2>Join Us Today:</h2>
+                    <p>Experience the power of our Library Management System and unlock the full potential of your
+                        library. Join us on this journey to create a dynamic, resourceful, and engaging environment for
+                        your library patrons. Together, we can make a positive impact on the lives of countless learners
+                        and knowledge seekers. Let's build a brighter future, one book at a time.</p>
+
+                </div>
+                <div class="col-md-6">
+                    <img src="http://themebubble.com/demo/marketingpro/wp-content/uploads/2016/10/seo-slide.png "
+                        alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="our-webcoderskull padding-lg">
+        <div class="container">
+            <div class="row heading heading-icon">
+                <h2>Our Team</h2>
+            </div>
+            <ul class="row">
+                <li class="col-12 col-md-6 col-lg-3">
+                    <div class="cnt-block equal-hight" style="height: 349px;">
+                        <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+                        </figure>
+                        <h3><a href="http://www.webcoderskull.com/">Neeraj</a></h3>
+                        <p>Assistant Librarian</p>
+                        <ul class="follow-us clearfix">
+                            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="col-12 col-md-6 col-lg-3">
+                    <div class="cnt-block equal-hight" style="height: 349px;">
+                        <figure><img src="http://www.webcoderskull.com/img/team1.png" class="img-responsive" alt="">
+                        </figure>
+                        <h3><a href="#">Satyam</a></h3>
+                        <p>Maintainance Seeker</p>
+                        <ul class="follow-us clearfix">
+                            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="col-12 col-md-6 col-lg-3">
+                    <div class="cnt-block equal-hight" style="height: 349px;">
+                        <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+                        </figure>
+                        <h3><a href="http://www.webcoderskull.com/">Swami </a></h3>
+                        <p>Book Manager</p>
+                        <ul class="follow-us clearfix">
+                            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="col-12 col-md-6 col-lg-3">
+                    <div class="cnt-block equal-hight" style="height: 349px;">
+                        <figure><img src="http://www.webcoderskull.com/img/team2.png" class="img-responsive" alt="">
+                        </figure>
+                        <h3><a href="http://www.webcoderskull.com/">Atul </a></h3>
+                        <p>HelpDesk Authority</p>
+                        <ul class="follow-us clearfix">
+                            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <?php include('contact_us.php') ?>
+    <?php include('footer.php') ?>
+</body>
+
+</html>
