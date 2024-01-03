350d77e0d87856294dc2b6f6ac8cbd3e2bd57d86
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-01-03 20:40:12 +0530

    Add files via upload

---
+++ User/change_password.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+if(!isset($_SESSION['email']))
+{
+	header("location:user_login.php");
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashboard</title>
+	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
+	<link rel="stylesheet" type="text/css" href="bootstrap-4.4.1/css/bootstrap.min.css">
+  	<script type="text/javascript" src="bootstrap-4.4.1/js/juqery_latest.js"></script>
+  	<script type="text/javascript" src="bootstrap-4.4.1/js/bootstrap.min.js"></script>
+	  <style>
+		body{
+			background-image: linear-gradient(-120deg, #d4fc79 0%, #96e6a1 100%);
+			background-image: linear-gradient(to right, #fc5c7d, #6a82fb);
+		background-attachment: fixed;
+		background-repeat: no-repeat;
+		}
+	</style
+</head>
+<body  style="background-color:grey">
+<?php include('navbar.php') ?>
+	<span><marquee style="background-color:violet">
+		<b>
+		<?php echo $_SESSION['name'];?> ,Looks like you want to change the Password.Please enter Your new password followed by current password.
+		</b>
+	</marquee></span><br><br>
+		<center><h4>Change Student Password</h4><br></center>
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<form action="update_password.php" method="post">
+					<div class="form-group">
+						<label for="password">Enter Current Password:</label>
+						<input type="password" class="form-control" name="old_password">
+					</div>
+					<div class="form-group">
+						<label for="New Password">Enter New Password:</label>
+						<input type="password" name="new_password" class="form-control">
+					</div>
+					<button type="submit" name="update" class="btn btn-primary">Update Password</button>
+				</form>
+			</div>
+			<div class="col-md-4"></div>
+		</div>
+</body>
+</html>
