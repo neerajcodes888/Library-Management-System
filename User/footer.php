7b39e46ac9c225e214fac3e13aaf32b14816e782
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-01-04 14:13:32 +0530

    Add files via upload

---
+++ User/footer.php
@@ -0,0 +1,148 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- Footer -->
+<style>
+        footer {
+          background: rgb(34,193,195);
+background: -moz-linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
+background: -webkit-linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
+background: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#22c1c3",endColorstr="#fdbb2d",GradientType=1);
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+        }
+</style>        
+<footer class="text-center">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:
+     
+      </span>
+    </div> -->
+    <!-- Left -->
+
+    <!-- Right -->
+    <!-- <div>
+     
+      <i class="bi bi-facebook"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+      <i class="bi bi-envelope-at-fill"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-github"></i>
+      </a>
+    </div> -->
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3 text-secondary"></i>LMS
+          </h6>
+          <p>
+          Serves as a comprehensive solution that empowers librarians and library staff to organize, track, and control the entire library collection, as well as provide excellent services to patrons.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful Links
+          </h6>
+          <p>
+            <a href="index.php" class="text-reset">Home</a>
+          </p>
+          <p>
+            <a href="user_login.php" class="text-reset">Login</a>
+          </p>
+          <p>
+            <a href="signup.php" class="text-reset">Sign Up</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset"></a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Social Handles
+          </h6>
+          <p>
+            <a href="https://www.facebook.com/neeraj.raj.184881?mibextid=ZbWKwL" class="me-4 link-secondary" target="blank">
+            <i class="fa-brands fa-facebook fa-bounce fa-2xl" style="color: #1f2e51;"></i>
+            </a>
+            <!-- <a href="#!" class="text-reset">Pricing</a> -->
+          </p>
+          <p>
+            <a href="https://github.com/neerajcodes888" class="text-reset" target="blank">
+            <i class="fa-brands fa-github fa-shake fa-2xl"></i>
+            </a>
+          </p>
+          <p>
+            <a href="https://instagram.com/_the_neeraj_official?igshid=NGExMmI2YTkyZg==" class="text-reset" target="blank"><i
+            <i class="fa-brands fa-instagram fa-beat-fade fa-2xl" style="color: #eb0000;"></i>
+              </a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+         
+          <p>
+            <a href="https://goo.gl/maps/HchhW1ZuiRbqyUPPA" target="blank">
+            <i class="fa-sharp fa-solid fa-location-dot fa-beat fa-2xl" style="color: #0b8ac1;"></i>
+            </i>
+            </a>
+          </p>
+          <p>
+
+            <a href="mailto:neerajmail888@gmail.com" class="text-reset" target="blank">
+            <i class="fa-regular fa-envelope fa-bounce fa-2xl" style="color: #e00f00;"></i>
+
+
+          </p>
+          <a href="tel:6031112298" target="blank">
+          <i class="fa-solid fa-phone fa-shake fa-2xl"></i>
+          </a>
+          </p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4">
+    © 2023 Copyright:
+    <a class="text-reset fw-bold" href="https://www.linkedin.com/in/%F0%9D%99%89%F0%9D%99%9A%F0%9D%99%9A%F0%9D%99%A7%F0%9D%99%96%F0%9D%99%9F-%F0%9D%99%86%F0%9D%99%AA%F0%9D%99%A2%F0%9D%99%96%F0%9D%99%A7-9a75811a2/" target="blank"><b
+        style="color:blue">Neeraj Kumar</b></a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
