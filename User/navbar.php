d5f51066a69fc3117baa6dc7259d988affd9fe07
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-01-05 01:00:13 +0530

    Add files via upload

---
+++ User/navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="user_dashboard.php">Library Management System (LMS)</a>
+			</div>
+			<font style="color: white"><span><strong>Welcome: <?php echo $_SESSION['name'];?></strong></span></font>
+			<font style="color: white"><span><strong>ID: <?php echo $_SESSION['id'];?></strong></font>
+		    <ul class="nav navbar-nav navbar-right">
+		      <li class="nav-item dropdown">
+	        	<a class="nav-link dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['name'];?></a>
+	        	<div class="dropdown-menu">
+	        		<a class="dropdown-item" href="view_profile.php">View Profile</a>
+	        		<div class="dropdown-divider"></div>
+	        		<a class="dropdown-item" href="edit_profile.php">Edit Profile</a>
+	        		<div class="dropdown-divider"></div>
+	        		<a class="dropdown-item" href="change_password.php">Change Password</a>
+
+					<div class="dropdown-divider"></div>
+	        		<a class="dropdown-item" href="feedback.php">Feedback & Complaint</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="feed_response.php">Feedback Response</a>
+	        	</div>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="logout.php">Logout</a>
+		      </li>
+		    </ul>
+		</div>
+	</nav>
