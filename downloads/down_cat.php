b10374df2442153626501ea3ff316cd2c6bc549a
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-01-05 16:42:22 +0530

    Add files via upload

---
+++ downloads/down_cat.php
@@ -0,0 +1,96 @@
+<?Php
+
+session_start();
+if(!isset($_SESSION['email']))
+{
+	die("Access denied");
+}
+require('../pdf/fpdf.php');
+
+function connectDB()
+{
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'lms';
+
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+}
+
+function getAdminData()
+{
+    $conn = connectDB();
+    $stmt = $conn->prepare("select name,email,mobile from admins where email='$_SESSION[email]'");
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+function getCatData()
+{
+    $conn = connectDB();
+    $stmt = $conn->prepare("select  * from category");
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->Image('../images/pdf_logo.png', 0, -1, 90);
+$pdf->Ln(5);
+$pdf->SetFont('Arial', 'B', 13);
+$pdf->Cell(40);
+$pdf->Cell(90, 10, 'Admin Profile', 1, 1, 'C');
+$pdf->Ln(3);
+
+$adminList = getAdminData();
+$pdf->Cell(20, 10, 'Name:', 0);
+foreach ($adminList as $admin) {
+
+    $pdf->Cell(20, 10, $admin['name'], 0);
+}
+$pdf->Ln();
+$pdf->Cell(20, 10, 'Email:', 0);
+foreach ($adminList as $admin) {
+
+
+    $pdf->Cell(20, 10, $admin['email'], 0); // Genre
+
+}
+$pdf->Ln();
+$pdf->Cell(20, 10, 'Phone:', 0);
+foreach ($adminList as $admin) {
+
+    $pdf->Cell(30, 10, $admin['mobile'], 0);
+
+    $pdf->Ln(); 
+}
+$pdf->Ln(10);
+$pdf->Cell(90, 10, 'Available Categories of Books', 1, 1, 'C');
+$pdf->Ln(3);
+$pdf->AliasNbPages();
+
+$pdf->SetFont('Arial', '', 12);
+
+$catList = getcatData();
+$pdf->Cell(50, 10, 'Category ID', 1); 
+$pdf->Cell(90, 10, 'Category Name.', 1); 
+
+$pdf->Ln();
+
+foreach ($catList as $cat) {
+
+    $pdf->Cell(50, 10, $cat['cat_id'], 1);
+    $pdf->Cell(90, 10, $cat['cat_name'], 1);
+    $pdf->Ln();
+}
+$pdf->Output('books_list.pdf', 'I');
+?>
