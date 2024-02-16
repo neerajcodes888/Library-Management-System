960d50f478c2c97c11427e0195e54b3914999716
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-16 15:33:12 +0530

    Update Regbooks.php

--- Admin/Regbooks.php
+++ Admin/Regbooks.php
@@ -2,8 +2,7 @@
 
 	session_start();
 	if(!isset($_SESSION['email']))
-{
-	die("Access denied");
+{die(include('../user/error.html'));
 }
 	#fetch data from database
 	$connection = mysqli_connect("localhost","root","");
