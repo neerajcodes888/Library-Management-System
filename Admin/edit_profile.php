c6ca86079844fc955875f6005751c4885432b1c8
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-16 15:36:52 +0530

    Update edit_profile.php

--- Admin/edit_profile.php
+++ Admin/edit_profile.php
@@ -4,7 +4,7 @@
 	session_start();
 	if(!isset($_SESSION['email']))
 {
-	die("Access denied");
+die(include('../user/error.html'));
 }
 	#fetch data from database
 	$connection = mysqli_connect("localhost","root","");
