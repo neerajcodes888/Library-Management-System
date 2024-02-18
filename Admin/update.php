73ae023e4c511ab711c8e18258c5b63f9964d94a
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-18 13:39:26 +0530

    Update update.php

--- Admin/update.php
+++ Admin/update.php
@@ -9,6 +9,7 @@ $query = "update admins set name = '$_POST[name]',email = '$_POST[email]',mobile
 $query_run = mysqli_query($connection,$query);
 ?>
 <script type="text/javascript">
+	
 	alert("Updated successfully...");
 	window.location.href = "admin_dashboard.php";
 </script>
