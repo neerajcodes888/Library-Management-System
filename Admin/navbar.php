75f0252acec41795833e58c6261675d4ae19d045
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-18 13:38:56 +0530

    Update navbar.php

--- Admin/navbar.php
+++ Admin/navbar.php
@@ -16,6 +16,7 @@
 	        		<a class="dropdown-item" href="change_password.php">Change Password</a>
                          		<div class="dropdown-divider"></div>
 	        		<a class="dropdown-item" href="../logout.php">Logout</a>
+				
 	        	</div>
 		      </li>
 		    </ul>
