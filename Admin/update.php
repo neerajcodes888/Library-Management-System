ebf6a85af001f48cc3383a5a26253dfd487f9582
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-18 13:29:57 +0530

    Update update.php

--- Admin/update.php
+++ Admin/update.php
@@ -6,7 +6,7 @@
 $connection = mysqli_connect("localhost","root","");
 $db = mysqli_select_db($connection,"lms");	
 $query = "update admins set name = '$_POST[name]',email = '$_POST[email]',mobile = '$_POST[mobile]'";
-	$query_run = mysqli_query($connection,$query);
+$query_run = mysqli_query($connection,$query);
 ?>
 <script type="text/javascript">
 	alert("Updated successfully...");
