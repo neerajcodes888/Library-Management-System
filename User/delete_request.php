0a2f135d804138e5de65ec1a37388c2a22338dd1
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-18 13:16:58 +0530

    Update delete_request.php

--- User/delete_request.php
+++ User/delete_request.php
@@ -4,7 +4,6 @@ if(!isset($_SESSION['email']))
 {
 	die(include('error.html'));
 }
-
 $connection = mysqli_connect("localhost","root","");
 $db = mysqli_select_db($connection,"lms");
 	$query = "delete from request_books where request_no = $_GET[rn]";
