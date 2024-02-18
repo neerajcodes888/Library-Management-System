e4600b51e7f6ff32726f39aa208274ef7a6b0f6e
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-18 13:29:46 +0530

    Update reject.php

--- Admin/reject.php
+++ Admin/reject.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-			<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>  
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>  
 	<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.min.css'></link>
 </head>
 <?php
