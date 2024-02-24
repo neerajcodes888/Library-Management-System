a81b344c2e33e641d565b169123dcb8a775df262
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-24 16:37:42 +0530

    Update delete_book.php

--- Admin/delete_book.php
+++ Admin/delete_book.php
@@ -11,6 +11,5 @@ $db = mysqli_select_db($connection,"lms");
 ?>
 <script type="text/javascript">
 	alert("Book Deleted successfully...");
-	window.location.href = "manage_book.php";
-	
+	window.location.href = "manage_book.php";	
 </script>
