40a4da1deb0ccb950defe1206578b44118e74553
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-24 16:42:08 +0530

    Update reject.php

--- Admin/reject.php
+++ Admin/reject.php
@@ -5,7 +5,6 @@
 </head>
 <?php
 $connection = mysqli_connect("localhost","root","");
-
 $db = mysqli_select_db($connection,"lms");
 	$query = "delete from request_books where request_no=  $_GET[rn]";
 	$query_run = mysqli_query($connection,$query);
