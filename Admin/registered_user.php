2ed9207c219f59e43dd5e0c8086dff26c2f35522
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-24 16:41:54 +0530

    Update registered_user.php

--- Admin/registered_user.php
+++ Admin/registered_user.php
@@ -6,7 +6,6 @@ $db = mysqli_select_db($connection,"lms");
 	$query = "select count(*) as user_count from users";
 	$query_run = mysqli_query($connection,$query);
 	while ($row = mysqli_fetch_assoc($query_run)){
-		$user_count = $row['user_count'];
-		
+		$user_count = $row['user_count'];	
 	}
 ?>
