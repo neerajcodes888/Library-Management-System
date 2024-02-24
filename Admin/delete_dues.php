311efb2892b6e4e3e8b94748a26fcc25fa6afcb8
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-24 16:37:59 +0530

    Update delete_dues.php

--- Admin/delete_dues.php
+++ Admin/delete_dues.php
@@ -8,7 +8,6 @@ $connection = mysqli_connect("localhost","root","");
 $db = mysqli_select_db($connection,"lms");
 	$query = "delete from issued_books where s_no=  $_GET[bn]";
 	$query_run = mysqli_query($connection,$query);
-
 ?>
 <script type="text/javascript">
 	alert("Dues Removed successfully...");
