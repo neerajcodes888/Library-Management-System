9d9929b9c6b179d6588ba0d17d7d982a789f9454
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-24 16:38:22 +0530

    Update delete_user.php

--- Admin/delete_user.php
+++ Admin/delete_user.php
@@ -8,7 +8,6 @@ $connection = mysqli_connect("localhost","root","");
 $db = mysqli_select_db($connection,"lms");
 	$query = "delete from users where id = '$_GET[id]'";
 	$query_run = mysqli_query($connection,$query);
-
 ?>
 <script type="text/javascript">
 	alert("User Deleted successfully...");
