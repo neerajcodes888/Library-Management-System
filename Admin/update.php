a922011a3080474737f6f98f7bdf362fb1ed14aa
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-24 16:42:22 +0530

    Update update.php

--- Admin/update.php
+++ Admin/update.php
@@ -1,8 +1,4 @@
 <?php
-// if(!isset($_SESSION['email']))
-// {
-// 	die("Access denied");
-// }
 $connection = mysqli_connect("localhost","root","");
 $db = mysqli_select_db($connection,"lms");	
 $query = "update admins set name = '$_POST[name]',email = '$_POST[email]',mobile = '$_POST[mobile]'";
