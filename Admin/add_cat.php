25e09f28af0c1192b591a01328d4bac5b6df04ca
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-24 16:36:37 +0530

    Update add_cat.php

--- Admin/add_cat.php
+++ Admin/add_cat.php
@@ -5,7 +5,7 @@
 {
 	die(include('../user/error.html'));
 }
-	#fetch data from database
+
 	$connection = mysqli_connect("localhost","root","");
 	$db = mysqli_select_db($connection,"lms");
 	$name = "";
