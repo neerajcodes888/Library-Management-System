b1d4aef2aa61e2c268285417b68f4cb1a255b52c
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-24 16:41:42 +0530

    Update registered_book.php

--- Admin/registered_book.php
+++ Admin/registered_book.php
@@ -5,8 +5,6 @@ $db = mysqli_select_db($connection,"lms");
 	$query = "select count(*) as book_count from books";
 	$query_run = mysqli_query($connection,$query);
 	while ($row = mysqli_fetch_assoc($query_run)){
-		$book_count = $row['book_count'];
-
-		
+		$book_count = $row['book_count'];	
 	}
 ?>
