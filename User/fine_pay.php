1e40964d27e6a1d67d1f744dbd0e2b1fe8307187
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-03-06 18:22:40 +0530

    Update fine_pay.php

--- User/fine_pay.php
+++ User/fine_pay.php
@@ -2,12 +2,12 @@
 session_start();
 if(!isset($_SESSION['email']))
 {
-	die(include('error.html'));
+	header("location:user_login.php");
 }
-$connection = mysqli_connect("localhost","root","");
-$db = mysqli_select_db($connection,"lms");
+$connection = mysqli_connect("localhost", "root", "");
+$db = mysqli_select_db($connection, "lms");
 $query = "select * from issued_books where book_no= $_GET[bn]";
-$query_run = mysqli_query($connection,$query);
+	$query_run = mysqli_query($connection,$query);
 if($row['dues_status'] == "Paid")
 {
 	?>
@@ -16,11 +16,14 @@ if($row['dues_status'] == "Paid")
 	window.location.href = "show_dues.php";
 </script>
 <?php
+
 }
 ?>
+
 else
 <?php
 {
+	
 	$query = "update issued_books set dues_status='Paid' where book_no= $_GET[bn]";
 	$query_run = mysqli_query($connection,$query);
 	$query = "update books set quantity=quantity+1 where book_no= $_GET[bn]";
