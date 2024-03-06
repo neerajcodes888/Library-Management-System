ab87a2b3e446e3c8ebefe5afc6a2cfb435ee8ece
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-03-06 18:26:21 +0530

    Update request.php

--- User/request.php
+++ User/request.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+// if(!isset($_SESSION['email']))
+// {
+// 	die("Access denied");
+// }
+require('functions.php');
+$connection = mysqli_connect("localhost", "root", "");
+$db = mysqli_select_db($connection, "lms");
+?>
+
 <!DOCTYPE html>
 <html>
 
