6c83ae815ba3239a944cc667995ef99b9235b519
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-03-06 12:49:17 +0000

    commit

--- User/change_password.php
+++ User/change_password.php
@@ -1,52 +1,175 @@
-<?php
-session_start();
-if(!isset($_SESSION['email']))
-{
-	die(include('error.html'));
-}
-?>
 <!DOCTYPE html>
 <html>
-	
 <head>
-	<title>Dashboard</title>
-	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
-	<link rel="stylesheet" type="text/css" href="bootstrap-4.4.1/css/bootstrap.min.css">
-  	<script type="text/javascript" src="bootstrap-4.4.1/js/juqery_latest.js"></script>
-  	<script type="text/javascript" src="bootstrap-4.4.1/js/bootstrap.min.js"></script>
-	  <style>
-		body{
-			background-image: linear-gradient(-120deg, #d4fc79 0%, #96e6a1 100%);
-			background-image: linear-gradient(to right, #fc5c7d, #6a82fb);
-		background-attachment: fixed;
-		background-repeat: no-repeat;
-		}
-	</style
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
+	<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.min.css'>
+	</link>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<style>
+
+body{
+    background: -webkit-linear-gradient(left, #0072ff, #00c6ff);
+}
+.contact-form{
+    background: #fff;
+    margin-top: 10%;
+    margin-bottom: 5%;
+    width: 70%;
+}
+.contact-form .form-control{
+    border-radius:1rem;
+}
+.contact-image{
+    text-align: center;
+}
+.contact-image img{
+    border-radius: 6rem;
+    width: 11%;
+    margin-top: -3%;
+    transform: rotate(29deg);
+}
+.contact-form form{
+    padding: 14%;
+}
+.contact-form form .row{
+    margin-bottom: -7%;
+}
+.contact-form h3{
+    margin-bottom: 8%;
+    margin-top: -10%;
+    text-align: center;
+    color: #0062cc;
+}
+.contact-form .btnContact {
+    width: 50%;
+    border: none;
+    border-radius: 1rem;
+    padding: 1.5%;
+    background: #dc3545;
+    font-weight: 600;
+    color: #fff;
+    cursor: pointer;
+}
+.btnContactSubmit
+{
+    width: 50%;
+    border-radius: 1rem;
+    padding: 1.5%;
+    color: #fff;
+    background-color: #0062cc;
+    border: none;
+    cursor: pointer;
+}
+    </style>
+
 </head>
-<body  style="background-color:grey">
-<?php include('navbar.php') ?>
-	<span><marquee style="background-color:violet">
-		<b>
-		<?php echo $_SESSION['name'];?> ,Looks like you want to change the Password.Please enter Your new password followed by current password.
-		</b>
-	</marquee></span><br><br>
-		<center><h4>Change Student Password</h4><br></center>
-		<div class="row">
-			<div class="col-md-4"></div>
-			<div class="col-md-4">
-				<form action="update_password.php" method="post">
-					<div class="form-group">
-						<label for="password">Enter Current Password:</label>
-						<input type="password" class="form-control" name="old_password">
-					</div>
-					<div class="form-group">
-						<label for="New Password">Enter New Password:</label>
-						<input type="password" name="new_password" class="form-control">
-					</div>
-					<button type="submit" name="update" class="btn btn-primary">Update Password</button>
-				</form>
-			</div>
-			<div class="col-md-4"></div>
-		</div>
+<body>
+<div class="container contact-form">
+            <div class="contact-image">
+                <img src="https://image.ibb.co/kUagtU/rocket_contact.png" alt="rocket_contact"/>
+            </div>
+            <form method="post">
+                <h3>Drop Us a Message</h3>
+               <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <input type="text" name="name" class="form-control" placeholder="Your Name *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="email" class="form-control" placeholder="Your Email *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="subject" class="form-control" placeholder="What's Your Subject *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" onclick="Submitted" name="submit" class="btnContact" value="Send Message" />
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <textarea name="msg" class="form-control" placeholder="Your Message *" style="width: 100%; height: 150px;"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </form>
+</div>
 </body>
 </html>
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require('email/Exception.php');
+require('email/SMTP.php');
+require('email/PHPMailer.php');
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+if(isset($_POST['submit']))
+{
+$name=$_POST['name'];
+$sub=$_POST['subject'];
+$email=$_POST['email'];
+$message=$_POST['msg'];
+
+
+    try {
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'aryaak323@gmail.com';                     //SMTP username
+        $mail->Password   = 'nnxjouocvevvwcop';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    
+        //Recipients
+        $mail->setFrom($email, 'Contact-us Response');
+        $mail->addAddress('neerajmail888@gmail.com');     //Add a recipient
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = $sub;
+        $mail->Body    = "<h3><b>Sender name -</b> $name <br> <b>Sender Email -</b> $email </h3><br><b>Message -</b> $message";
+        
+    
+        if($mail->send()){
+           
+        
+        
+	?>
+								<script type="text/javascript">
+								swal({
+          title: 'Message Sent!',
+          text: 'Response sent to the Admin!',
+          type: 'success'
+        }).then(function() {
+            window.location.href = "about_us.php";
+        })
+								</script>
+								<?php
+        }else{
+            	?>
+								<script type="text/javascript">
+								swal({
+          title: 'Invalid Email!',
+          text: 'Please enter a valid Email Address!',
+          type: 'warning'
+        }).then(function() {
+            window.location.href = "about_us.php";
+        })
+								</script>
+								<?php
+        }
+       // echo 'Message has been sent';
+    } catch (Exception $e) {
+       // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+?>
